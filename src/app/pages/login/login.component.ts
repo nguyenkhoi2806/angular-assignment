9333f70f5c3896a5f0fd6b736e853d15761e812e
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2024-06-15 16:02:02 +0700

    feat: update form validator

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,35 +1,60 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
+import { Router, RouterModule } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { AuthService } from 'app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, RouterOutlet, RouterModule],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  email: string = '';
-  password: string = '';
-  isSubmitTing: boolean = false;
+  loginForm: FormGroup;
+  isSubmitting: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 
   login(): void {
-    this.isSubmitTing = true;
-    this.authService.login(this.email, this.password).subscribe(
-      (response) => {
-        if (response.access_token) {
-          this.router.navigate(['/dashboard']);
+    if (this.loginForm.valid) {
+      this.isSubmitting = true;
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email, password).subscribe(
+        (response) => {
+          if (response.access_token) {
+            this.router.navigate(['/home']);
+          }
+        },
+        (error) => {
+          this.isSubmitting = false;
+          console.error(error);
+          alert('Login failed');
         }
-      },
-      (error) => {
-        this.isSubmitTing = false;
-        console.error(error);
-        alert('Login failed');
-      }
-    );
+      );
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
   }
 }
