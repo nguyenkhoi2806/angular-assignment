6e3aed096353f618ab0ee763d95432de90341a09
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2024-06-15 15:55:37 +0700

    feat: update sign up layout

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 import { AuthService } from 'app/services/auth/auth.service';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterOutlet, RouterModule],
   templateUrl: './signup.component.html',
 })
 export class SignupComponent {
